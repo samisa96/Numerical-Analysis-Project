bc5500678470525b00ce0f35a5ab0e22d0e921aa
Yariv Garala <yariv1052@gmail.com>
2018-12-03 18:03:11 +0200

    Upload - Hackathon

---
+++ Lib/Hackathon.py
@@ -0,0 +1,10 @@
+from Bisection.py import *
+from N_R import *
+from math import sqrt
+
+def Find_Rij(point_i,point_j,h):
+    return sqrt(((point_i[0]-point_j[0])**2)+((point_i[1]-point_j[1])**2)+(h**2))
+def Calculate_Dij(c,k,u,h,point_i,point_j):
+    return (c*(1+k*h)*exp(-u*Distance_Rij(point_i,point_j,h)))/(Distance_Rij(point_i,point_j,h)**2)
+def Find_c(func):
+    roots1=
