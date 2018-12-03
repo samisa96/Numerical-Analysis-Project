83f8478b9bdd31e3c2e0a71a5e4e1ceea5de5bf4
Yariv Garala <yariv1052@gmail.com>
2018-12-03 18:01:49 +0200

    update

--- Lib/GaussSiedle_SOR.py
+++ Lib/GaussSiedle_SOR.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def iterative(matrix, b, c):
+
+def iterative(matrix, b, c, printList):
     result = []
     sum = 0
     count = 0
@@ -11,7 +12,7 @@ def iterative(matrix, b, c):
     for i in range(len(matrix)):
         result.append(0)
 
-    while(flag2 == 0 and count <= 1000):
+    while(flag2 == 0 and count <= 100):
         for i in range(len(matrix)):
             for j in range(len(matrix)):
                 sum = sum + (matrix.item(i, j)*result[j])
@@ -22,11 +23,15 @@ def iterative(matrix, b, c):
                 flag = 1
             result[i] = sum
             sum = 0
+
         if flag == 0:
             flag2 = 1
         else:
             flag = 0
         count = count + 1
+        printList.append(count)
+        for j in result:
+            printList.append(j)
 
     return result
 
@@ -126,51 +131,25 @@ def create_U(matrix):
     U = np.matrix(result)
     return U
 
-def normMax(mat):
-    '''
-    :param mat: matrix
-    :return: Max Norm
-    '''
-    sum = 0
-    list = []
-
-    for i in range(len(mat)):
-        for j in range(len(mat.getA().item(0))):
-            sum = sum + abs(mat.item(i)[j])
-        list.append(sum)
-        sum = 0
-    return(max(list))
-
-def calcDominant(mat):
-    mat = np.matrix(mat)
-    newOrder = list(range(len(mat)))
-    found = False
-    for n in range(len(mat)):
-        for m in range(n, len(mat)):
-            if (abs(mat[n, m]) > abs(np.sum(mat[n, :]) - mat[n, m])):
-                newOrder.pop(m)
-                newOrder.insert(n, m)
-                found = True
-        if not found:
-            return None
-        mat = mat[:, newOrder]
-        newOrder = list(range(len(mat)))
-        found = False
-    return mat
-
 def SOR(matrix, b):
     flag = 0
     w = 0.05
     while flag == 0 and w <= 2:
+        printList = []
         D = create_D(matrix)
+        print("D:",D)
         L = create_L(matrix)
+        print("L:",L)
         U = create_U(matrix)
+        print("U:",U)
 
         W_L = multi_scalar(L, w)
+        print("W_L:",W_L)
         D_WL = sum_m(W_L, D)
+        print("D_WL:",D_WL)
 
         W_D = multi_scalar(D, 1 - w)
-
+        print("W_D:",W_D)
         W_U = multi_scalar(U, w)
 
         new_b = []
@@ -192,7 +171,7 @@ def SOR(matrix, b):
         for i in range(len(matrix)):
             vector.append(D.item(i, i))
 
-        r = iterative(mat, vector, new_b)
+        r = iterative(mat, vector, new_b, printList)
         close = True
         for i in range(len(matrix)):
             if is_close(matrix[i], b[i], r, 0.0001) == False:
@@ -202,10 +181,18 @@ def SOR(matrix, b):
             w = w + 0.05
         else:
             flag = 1
+            PrintList(printList)
 
     return r
 
-def GAUSS(matrix, b):
+def PrintList(printList):
+    for i in range(0 ,len(printList), 4):
+        print("number iteration:", printList[i])
+        for j in range(1 , 4):
+            print("x", j, ": ", printList[i + j])
+
+
+def gaus(matrix, b):
     D = create_D(matrix)
     L = create_L(matrix)
     U = create_U(matrix)
@@ -221,22 +208,62 @@ def GAUSS(matrix, b):
         new_b.append(1.0)
 
     L_D_b = L_D_b.getA()[0]
-    r = iterative(L_D_U_1, new_b ,L_D_b)
+
+    printList = []
+    r = iterative(L_D_U_1, new_b ,L_D_b, printList)
+    PrintList(printList)
     return r
 
+def normMax(mat):
+    '''
+    :param matrixList: matrix
+    :param size: Size of matrix
+    :return: Max Norm
+    '''
+    sum = 0
+    list = []
+
+    for i in range(len(mat)):
+        for j in range(len(mat)):
+            sum = sum + abs(mat.item(i, j))
+        list.append(sum)
+        sum = 0
+    return(max(list))
+
+def calcDominant(mat):
+    mat = np.matrix(mat)
+    newOrder = list(range(len(mat)))
+    found = False
+    for n in range(len(mat)):
+        for m in range(n, len(mat)):
+            if (abs(mat[n, m]) > abs(np.sum(mat[n, :]) - mat[n, m])):
+                newOrder.pop(m)
+                newOrder.insert(n, m)
+                found = True
+        if not found:
+            return mat
+        mat = mat[:, newOrder]
+        newOrder = list(range(len(mat)))
+        found = False
+    return mat
+
+
 if __name__ == '__main__':
-
-    """norm_A = normMax(mat)
-    print(norm_A)
-    A_inv = mat.I
-    norm_A_inv = normMax(A_inv)
-    cond = norm_A * norm_A_inv
-    print("cond: ", cond) """
-
-    a = np.matrix([[8, 2, 4], [3, 10, -5], [5, 2, 9]])
+    a = np.matrix([[2, 7, 4], [10, 2, -5], [3, 4, 9]])
     b = [-7, 3, -3.5]
 
-    #new_mat = calcDominant(a)
+    norm_A = normMax(a)
+    try:
+        A_inv = a.I
+        norm_A_inv = normMax(A_inv)
+        cond = norm_A * norm_A_inv
+        print("cond: ", cond)
 
-    #print(SOR(a, b))
-    print(GAUSS(a, b))
+    except(np.linalg.LinAlgError):
+        print("no inverse")
+
+    a = calcDominant(a)
+    print(a)
+
+    print(SOR(a, b))
+    #print(gaus(a, b))
