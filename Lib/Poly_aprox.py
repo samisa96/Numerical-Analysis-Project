cc8bf2ec174f83d094fc401e3e0e81c96cc6409e
shirelBiton <44835615+shirelBiton@users.noreply.github.com>
2019-01-01 20:32:16 +0200

    Update Poly_aprox.py

--- Lib/Poly_aprox.py
+++ Lib/Poly_aprox.py
@@ -1,13 +1,32 @@
 import numpy as np
-import GaussSiedle_SOR as gs
+import SOR as gs
+
+
+def StrongcalcDominant(mat):
+    mat = np.matrix(mat)
+    newOrder = list(range(len(mat)))
+    found = False
+    for n in range(len(mat)):
+        for m in range(n, len(mat)):
+            if (abs(mat[n, m]) > abs(np.sum(mat[n, :]) - mat[n, m])):
+                newOrder.pop(m)
+                newOrder.insert(n, m)
+                found = True
+        if not found:
+            return None
+        mat = mat[:, newOrder]
+        newOrder = list(range(len(mat)))
+        found=False
+    return mat
 
 def calcDominant(mat,b):
     mat = np.matrix(mat)
     newOrder = list(range(len(mat)))
     found = False
     for m in range(len(mat)):
+        c = []
         for n in range(m, len(mat)):
-            if (abs(mat[n, m]) > abs(np.sum(mat[n:, m]) - mat[n, m])):
+            if (abs(mat[n, m]) >= abs(np.sum(mat[n:, m]) - mat[n, m])):
                 newOrder.pop(n)
                 newOrder.insert(m, n)
                 found = True
@@ -15,7 +34,9 @@ def calcDominant(mat,b):
         if not found:
             return None
         mat = mat[newOrder,:]
-        b=b[newOrder]
+        for i in range(len(b)):
+            c.append(b[newOrder[i]])
+        b = c
         newOrder = list(range(len(mat)))
         found=False
     return mat,b
@@ -30,12 +51,29 @@ def polynomial_approx(x, y):
         mat.append(row)
         row = []
     mat = np.matrix(mat)
-    y = [[10.5],[ 6.1], [3.5]]
-    mat,y= calcDominant(mat,np.mat(y))
-    return gs.SOR(mat,y)
+    mat,y= dominant(mat,y)
+    y = gs.gaus(mat, y)
+    return y
 
 def polynomial_calc(p, num, pow):
     sum = 0
     for i in range(pow + 1):
         sum = sum + p[i].item(0,0) * (num ** (pow - i))
     return sum
+
+def dominant(mat, b):
+    new_mat = StrongcalcDominant(mat)
+    if type(new_mat) == type(None):
+        new_mat = calcDominant(mat, b)
+        if new_mat != None:
+           return new_mat
+    else:
+        return new_mat, b
+    return mat, b
+
+if __name__ == "__main__":
+    x = [2,1,0]
+    b = [-14, -4, 0]
+
+    r = polynomial_approx(x, b)
+    print(r)
