d810d13b4168916ee3ab1f823e2f737f1290f491
shirelBiton <44835615+shirelBiton@users.noreply.github.com>
2019-01-03 14:23:21 +0200

    Update Neville_method.py

--- Lib/Neville_method.py
+++ Lib/Neville_method.py
@@ -1,8 +1,8 @@
 def init_poly(x, x_value, y_value, index):
-    return (y_value[index[1]] * (x_value[index[0]] - x) - y_value[index[0]] * (x_value[index[1]] - x)) / (x_value[index[0]] - x_value[index[1]])
+    return (y_value[index[1]] * (x - x_value[index[0]]) - y_value[index[0]] * (x - x_value[index[1]])) / (x_value[index[1]] - x_value[index[0]])
 
 def poly_at_index(x, x_value, poly_index, index):
-    return (poly_index[1] * (x_value[index[0]] - x) - poly_index[0] * (x_value[index[1]] - x)) / (x_value[index[0]] - x_value[index[1]])
+    return (poly_index[1] * (x - x_value[index[0]]) - poly_index[0] * (x - x_value[index[1]])) / (x_value[index[1]] - x_value[index[0]])
 
 def nevil(x, x_value, y_value):
     polyindex = {}
@@ -68,7 +68,12 @@ if '__main__' == __name__:
     nevil(x, x_value, y_value) '''
 
     # Example 5:
-    x = 0.5
+    '''x = 0.5
     x_value = [0, 1.5, -5, 2, 5]
     y_value = [2, 6, 4, 2, 5]
+    nevil(x, x_value, y_value)'''
+
+    x = 3
+    x_value = [1, 2, 4]
+    y_value = [1, 0, 1.5]
     nevil(x, x_value, y_value)
