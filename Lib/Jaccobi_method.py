c16d3d20184c8d80b7eaf2ccd68726e9b36dbb72
shirelBiton <44835615+shirelBiton@users.noreply.github.com>
2019-01-05 18:57:43 +0200

    Update Jaccobi_method.py

--- Lib/Jaccobi_method.py
+++ Lib/Jaccobi_method.py
@@ -130,10 +130,10 @@ def create_U(matrix):
     U = np.matrix(result)
     return U
 
-def PrintList(printList):
-    for i in range(0 ,len(printList)-1, 4):
+def PrintList(printList, size):
+    for i in range(0 ,len(printList)-1, size):
         print("number iteration:", printList[i])
-        for j in range(1 , 4):
+        for j in range(1 , size):
             print("x", j, ": ", printList[i + j])
 
 def jacobi(matrix, b):
@@ -155,7 +155,7 @@ def jacobi(matrix, b):
 
     printList = []
     r = iterative(L_U_D_1, new_b ,L_D_b, printList)
-    PrintList(printList)
+    PrintList(printList, len(r) + 1)
     return r
 
 def StrongcalcDominant(mat):
